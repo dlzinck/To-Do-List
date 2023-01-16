74eb64863f021331b6b1e853b1de32d2589b0876
Drew Zinck <dlzincktheii@gmail.com>
2023-01-15 18:01:01 -0700

    Completed the UI

--- src/App.js
+++ src/App.js
@@ -3,18 +3,59 @@ import './App.css';
 import styled from "styled-components";
 
 const Container = styled.div`
-display: flex;
-align-items: center;
-flex-direction: column;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
+
+const Button = styled.button`
+  display: inline-block;
+  flex: 1;
+  border: none;
+  background-color: green;
+  color: white;
+  height: 30px;
+  width: 50px;
+  boarder-radius: 2px;
+  cursor: pointer;
+`;
+
+const Text = styled.input`
+  border: 2px solid #000;
+`;
+
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+
+const Tasks = styled.span`
+`;
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        
-      </Container>
-    </div>
+    <Container>
+      <div>
+        <h1>To-Do List</h1>
+        <Text> value={input}</Text>
+        <Button>Add</Button>
+        <Tasks>
+          <TaskCount>
+            <b>Pending</b>
+          </TaskCount>
+          <TaskCount>
+           <b>Completed</b>
+         </TaskCount>
+       </Tasks>
+        <div>
+           <ul>
+            {/* List items of tasks will go HERE */}
+          </ul>
+        </div>
+       <Button>Clear</Button>
+     </div>
+    </Container>
   );
 }
 
